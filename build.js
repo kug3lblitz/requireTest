8d0bef686ea85fe9d4096e6d0a1b4cc640c19804
Patrick Werner <pwerner2@gmail.com>
2015-06-14 00:01:11 -0400

    added commentary, as I should have earlier

--- build.js
+++ build.js
@@ -1,16 +1,24 @@
 ({
 	baseUrl: ".",
+
+    //loads client side config for default paths and shims
 	mainConfigFile: "main.js",
 	out: "bundle.js",
 	name: "main",
-	optimize: "uglify2",
+	optimize: "uglify2"
+
+    //paths for r.js
 	paths: {
 		jquery: "empty:",
 		foundation: "bower_components/foundation/js/foundation"
 	},
-	excludeShallow: ['jquery'],
+
+    //jquery not in bundle, loaded from cdn @ runtime
+    wrapShim: true,
+	excludeShallow: ['jquery']
+
+    //generate sourcemaps
 	generateSourceMaps: true,
 	useSourceUrl: true,
-	preserveLicenseComments: false,
-    wrapShim: true
+	preserveLicenseComments: false
 })
