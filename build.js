418712fe90b3edb038583e9010713d572a6a53eb
Patrick Werner <pwerner2@gmail.com>
2015-06-13 22:34:14 -0400

    added bundle files to gitignore

--- build.js
+++ build.js
@@ -1,15 +1,15 @@
 ({
-    preserveLicenseComments: false,
-    baseUrl: ".",
-    out: "bundle.js",
-    name: "main",
-    optimize: "uglify2",
-    paths: {
-        jquery: "empty:",
-        foundation: "bower_components/foundation/js/foundation",
-    },
-    mainConfigFile: "main.js",
-    generateSourceMaps: true,
-    useSourceUrl: true
+	baseUrl: ".",
+	mainConfigFile: "main.js",
+	out: "bundle.js",
+	name: "main",
+	optimize: "uglify2",
+	paths: {
+		jquery: "empty:",
+		foundation: "bower_components/foundation/js/foundation"
+	},
+	excludeShallow: ['jquery'],
+	generateSourceMaps: true,
+	useSourceUrl: true,
+	preserveLicenseComments: false
 })
-
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 requirejs.config({
-    baseUrl: '.',
-    paths: {
-        foundation: [ 'bower_components/foundation/js/foundation' ],
-        jquery: [ 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js' ]
-    },
-    shim: {
-        foundation: {
-            deps: ['jquery'],
-            exports: 'Foundation'
-        },
+	baseUrl: '.',
+	paths: {
+		foundation: 'bower_components/foundation/js/foundation',
+		jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min'
 	},
+	shim: {
+		foundation: {
+			deps: ['jquery'],
+			exports: 'Foundation'
+		}
+	}
 });
 
 
